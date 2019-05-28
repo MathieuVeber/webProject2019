555c7e8fb5e3152f2d09e4b0439d0473ff76cecb
MathieuVeber <43341681+MathieuVeber@users.noreply.github.com>
2019-05-28 16:27:13 +0200

    Patch presentation responsive

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -57,10 +57,10 @@
 
 <div class="row">
 
-  <div class="col-5">
+  <div class="col-md-4">
   </div>
 
-  <div class="col-2">
+  <div class="col-12 col-md-4">
     <div class="row justify-content-center">
       {{ $cars->links() }}
     </div>
